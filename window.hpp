609d04ceb848314848b3f5f50241bb42c194ca85
Alex <arpearce93@gmail.com>
2020-05-18 02:25:19 +0100

    LeftCol and RightCol length fixes

--- window.hpp
+++ window.hpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 const int windowWidth = 1000;
-const int windowHeight = 1000; //config? Probably not, just set to reasonable values
+const int windowHeight = 700; //config? Probably not, just set to reasonable values
 const int cellSize = 100;
 const int columnCount = windowHeight / cellSize; //used to calculate the next horizontal cell
 const int rowCount = windowWidth / cellSize;
@@ -13,8 +13,8 @@ const int screenWidth = sf::VideoMode::getDesktopMode().width - windowWidth;
 const int screenHeight = sf::VideoMode::getDesktopMode().height - windowHeight;
 int topRow[rowCount];
 int bottomRow[rowCount];
-int leftCol[rowCount];
-int rightCol[rowCount];
+int leftCol[columnCount];
+int rightCol[columnCount];
 
 sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Snake");
 board b = board(windowWidth, windowHeight, cellSize);
