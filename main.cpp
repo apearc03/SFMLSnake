1b87518e15f15446f196d2413f229fa284cddb7e
Alex <arpearce93@gmail.com>
2020-05-22 12:10:25 +0100

    Command line arguments finished

--- main.cpp
+++ main.cpp
@@ -5,22 +5,33 @@
 
 int main(int argc, char *argv[])
 {
+    const char *programInfo = 
+    "SFML Snake by Alex Pearce\n"
+    "Program must be run with all arguments or none at all and defaults are applied.\n"
+
+    "Usage: ./sfmlSnake [windowWidth] [windowHeight] [cellSize] [frameRate]\n"
+    "windowWidth: Any integer\n"
+    "windowHeight: Any integer\n"
+    "cellSize: An integer that must be a factor of both windowWidth and windowHeight\n"
+    "frameRate: An integer that must be between 10 and 60, inclusive of both.\n"
+
+    "Default values are 1000, 800, 50, 15 which are applied when no arguments are specified.\n"
+    "Your score is shown after each round ends.\n";
     if (argc == 1)
     {
-        game g(1000, 1000, 100, 10);
+        game g(1000, 800, 50, 15);
         g.start();
         return 0;
     }
     std::string argOne = std::string(argv[1]);
     if (argOne.find("help") != std::string::npos || argOne == "-h")
     {
-        std::cout << "help found";
-        //echo argument info and controls. Score printed after game over or the player wins. Defaults with no arguments.
+        std::cout << programInfo;
         return 0;
     }
     if (argc != 5)
     {
-        //incorrect number of arguments.
+        std::cout << "Incorrect number of arguments, use the -help flag for more information\n";
         return 0;
     }
     std::regex e("^-?\\d+");
@@ -29,8 +40,6 @@ int main(int argc, char *argv[])
     std::string argFour = std::string(argv[4]);
     if (std::regex_match(argOne, e) && std::regex_match(argTwo, e) && std::regex_match(argThree, e) && std::regex_match(argFour, e))
     {
-        std::cout << "all numbers";
-        //check width and height are multiples and frame rate is between 10 and 60. Inclusive.
         int width = std::stoi(argOne);
         int height = std::stoi(argTwo);
         int cellSize = std::stoi(argThree);
@@ -43,13 +52,13 @@ int main(int argc, char *argv[])
         }
         else
         {
-            //print that width and height must be multiples of cellSize and framerate must be between 10 and 60.
+            std::cout << "width and height must be multiples of cellSize and framerate must be between 10 and 60, inclusive, use the -help flag for more information\n";
             return 0;
         }
     }
     else
     {
-        //print that all arguments must be numbers.
+        std::cout << "All arguments must be valid integers, use the -help flag for more information\n";
         return 0;
     }
 }
